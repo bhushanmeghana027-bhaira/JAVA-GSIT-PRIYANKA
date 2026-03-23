adfe116f3a3c4464d193b9e50370be960ede087b
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-03-23 08:45:55 +0530

    just for check

---
+++ src/main/java/day4/IfElse.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(7>18) {
+			System.out.println("true age");
+			
+		}
+		else {
+			System.out.println("wrong age");
+		}
+	}
+
+}
