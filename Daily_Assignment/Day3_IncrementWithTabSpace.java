13627dc88b1f745e2a4915b4ea9346fd506556b1
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-03-23 23:14:35 +0530

    PHW3:Variable print in one line using escape sequence its tricky

---
+++ Daily_Assignment/Day3_IncrementWithTabSpace.java
@@ -0,0 +1,11 @@
+
+public class Day3_IncrementWithTabSpace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =5;
+		System.out.println("Value of \"a\":" + a + "\nAfter increment:" +"    "+ (a + 2));
+//		"\t" didnt work 
+	}
+
+}
--- src/main/java/day3/VariableNaming.java
+++ src/main/java/day3/VariableNaming.java
@@ -5,6 +5,15 @@ public class VariableNaming {
 		System.out.println("class name "+":"+"Pascal case"+ "all first letter caps");
 		System.out.println("variable and method  name"+":"+"Camel case"+"only first letter small ");
 		System.out.println("package name"+":"+"all small");
+		int num1;
+//		int 1num; // wrong not allowed	
+		int $num;
+		int __num;
+		int num_;
+//		int num.of; // wrong not allowed
+		int numOfPeoplePresenetToday; //camelcase
+		int NumOfPeoplePresentToday;//pascalCase
+		
 	}
 
 }
