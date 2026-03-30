afebf860b4f148db9422b4745c2da9842d66567c
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-03-31 00:57:55 +0530

    PHW7:PRIME

---
+++ Daily_Assignment/Day7_Primer.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Day7_Primer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number :");
+		int n = sc.nextInt();
+		int count = 0;
+		
+		for(int i=1;i<=n;i++) {
+			if(n%i==0) {
+				count++;
+			}
+		}
+		if(count==2) {
+			System.out.println(n+":"+"is a prime");
+		}
+		else
+		{
+			System.out.println(n+":"+" not a prime");
+		}
+
+	}
+
+}
