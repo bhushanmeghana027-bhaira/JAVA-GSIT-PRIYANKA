275b2a6fa815071cd02caef1fd781b63b9c4d89b
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-04-01 07:06:49 +0530

    March31:Patterns homework

---
+++ Daily_Assignment/March31_pattern_DecreaseTriangle/DecreasingTriangle.java
@@ -0,0 +1,17 @@
+package March31_pattern_DecreaseTriangle;
+
+public class DecreasingTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=i;j<=n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
