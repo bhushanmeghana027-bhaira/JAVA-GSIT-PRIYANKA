0ae9e3f3571657e57ec81c3c682d2f1182cf3764
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-04-06 22:40:45 +0530

    Weekly assignment on array

--- Weekly_Assignment/Week4_Arrays/ZeroMovers.java
+++ Weekly_Assignment/Week4_Arrays/ZeroMovers.java
@@ -4,12 +4,19 @@ public class ZeroMovers {
 	int[] zeroMovers(int[] arr) {
 		int pos=0;
 		int[] arr1 = new int[5];
+//		for(int i=0;i<=arr.length-1;i++) {
+//			if(arr[i]==0) {
+//				arr1[pos] = arr[i];
+//				pos++;
+//			}
+//		}
 		for(int i=0;i<=arr.length-1;i++) {
 			if(arr[i]!=0) {
 				arr1[pos] = arr[i];
 				pos++;
 			}
 		}
+		
 		
 		return arr1 ;
 	}
